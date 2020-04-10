3d4a36e0a35c0df3a4fb82ede8cb81b3fe339033
mcconnekeelanr <62781163+mcconnekeelanr@users.noreply.github.com>
2020-04-10 14:48:20 -0400

    Add files via upload

--- assaignment1.py
+++ assaignment1.py
@@ -2,8 +2,9 @@
 
 fib0=0          #this is the variable to start at 0
 p=0             #this is a place holder for fib0
-fib1=1          #this is the variable to start at 1
+fib1=0        #this is the variable to start at 1
 sum1=0          #variable equaling fib1 and fib0 
+
 #print("ok here we go")  #just some random thing i thought would be cool before the loop starts
 #while sum1<50:   #while loop idk 
  #   print(sum1)     #prints everytime the loop finishes
@@ -13,23 +14,32 @@ sum1=0          #variable equaling fib1 and fib0
     #    fib1=fib0+fib1      #adds fib0-1
      #   fib0=p              #gives fib0 the falue of p which was fib1 before math
         
-f=open("krm-hw","a")
-print("lets ago")
-while sum1<200000:             # i condensed the above code into this and added writing to file
-    print(sum1
-            )
-    sum1=fib1+fib0
-    p=fib1
-    fib1=fib0+fib1
-    fib0=p
-    f.write(str(sum1))
-    
 
 
 
-#y=open("krm-hw","w")
-#y.write("random sample data")
-#y.close(
-
+import argparse             #imports argparse
+parser= argparse.ArgumentParser()   #begins the module
+parser.add_argument("num",help="please type a int here",type=int) #adds the argument for num
+args = parser.parse_args()                       #variable args holds the parse argument   
+print(args.num)                                 #prints args with the variable argument num
+f=open("krm-hw","w")                            #opens krm-hw to write to
+fib1=args.num                                   #variables 
+dib0=args.num
+print("do you want to overwrite?")
+answer=input("yes or no"+ " ")                       #userinput to overwrite or not
+if answer=="no":
+    f.close()
+    print("no? ok canceling")
+        
+else:
+    print("yes?, ok here we go")
+    while sum1<1000000:          #condensed the above code into this and added writing to file
+        print(sum1)                             
+        sum1=fib1+fib0
+        p=fib1
+        fib1=fib0+fib1
+        fib0=p
+        f.write(str(sum1))
+        f.write(",")
 
 
